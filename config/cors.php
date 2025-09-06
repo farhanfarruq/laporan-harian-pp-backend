cc17906d983607733692c99a0dd03604a2b3aff6
farhanfarruq <anakpcd987@gmail.com>
2025-09-07 04:37:58 +0700

    deploymnet

--- config/cors.php
+++ config/cors.php
@@ -19,8 +19,8 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:5173', 'http://127.0.0.1:5173'],
-
+    'allowed_origins' => ['https://laporan-harian-pp-frontend.vercel.app'],
+    
     'allowed_origins_patterns' => [],
 
     'allowed_headers' => ['*'],
