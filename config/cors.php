000ac52dddbabedba94b102b477f8f4c4fcfb20d
farhanfarruq <anakpcd987@gmail.com>
2025-09-07 14:11:16 +0700

    update cors

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['https://laporan-harian-pp-frontend.vercel.app'],
+    'allowed_origins' => ['*'],
     
     'allowed_origins_patterns' => [],
 
